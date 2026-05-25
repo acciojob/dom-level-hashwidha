0b5f126bc4334cf1f64a8080733ec66fba03d361
hashwidha <147316086+hashwidha@users.noreply.github.com>
2026-05-25 12:59:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-let element=document.queryselector(#level);
+let element=document.getElementById("#level");
 let level=0;
 while(element.parentElement){
 	level++;
